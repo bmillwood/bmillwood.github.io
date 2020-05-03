ebc3cc57b8f66824fb83e7d1664cef4135193e50
Ben Millwood <thebenmachine+git@gmail.com>
2020-05-03 13:05:25 +0800

    better indentation + styling

--- js-toys/editable-list/with-just-react/editable-list.js
+++ js-toys/editable-list/with-just-react/editable-list.js
@@ -1,123 +1,121 @@
 'use strict';
 
 function ELNode(props) {
-    var content;
-    if(props.editing) {
-        content = (
-                <input type="text"
-            defaultValue={props.content}
-            onKeyPress={props.handleKey}
-            onChange={props.handleChange}
-                />
-        );
-    } else {
-        content = (
-                <span>
-            {props.content}
-            <span className="buttons">
-                <button className="edit" onClick={props.startEditing}>edit</button>
-                <button className="child" onClick={props.addChild}>child</button>
-                </span>
-            </span>
-        );
-    }
-    if(props.children.length == 0) {
-        return <div>{content}</div>;
-    } else {
-        var children = props.children.map(child => <li>{child}</li>);
-        return (
-                <div>
-                {content}
-                <ul>
-                {children}
-            </ul>
-                </div>
-        );
-    }
+  var content;
+  if(props.editing) {
+    content = (
+      <input type="text"
+      defaultValue={props.content}
+      onKeyPress={props.handleKey}
+      onChange={props.handleChange}
+      />
+    );
+  } else {
+    content = (
+      <span>
+      <button className="edit" onClick={props.startEditing}>edit</button>
+      <button className="child" onClick={props.addChild}>child</button>
+      {props.content}
+      </span>
+    );
+  }
+  if(props.children.length == 0) {
+    return <div>{content}</div>;
+  } else {
+    var children = props.children.map(child => <li>{child}</li>);
+    return (
+      <div>
+      {content}
+      <ul>
+      {children}
+      </ul>
+      </div>
+    );
+  }
 }
 
 function newNode(initialContent) {
-    return {
-        nextFreeChild: 0,
-        content: initialContent,
-        editing: false,
-        children: {},
-        childrenIds: [],
-    };
+  return {
+    nextFreeChild: 0,
+    content: initialContent,
+    editing: false,
+    children: {},
+    childrenIds: [],
+  };
 }
 
 class EditableList extends React.Component {
-    constructor(props) {
-        super(props);
-        const root = newNode('root');
-        this.state = { root: root };
-    }
+  constructor(props) {
+    super(props);
+    const root = newNode('root');
+    this.state = { root: root };
+  }
 
-    getAt(path) {
-        var node = this.state.root;
-        for(var i = 0; i < path.length; i++) {
-            node = node.children[path[i]];
-        }
-        return node;
+  getAt(path) {
+    var node = this.state.root;
+    for(var i = 0; i < path.length; i++) {
+      node = node.children[path[i]];
     }
+    return node;
+  }
 
-    editAt(path, f) {
-        var newRoot = Object.assign({}, this.state.root);
-        var node = newRoot;
-        for(var i = 0; i < path.length; i++) {
-            const ci = path[i];
-            node.children = Object.assign({}, node.children);
-            node.children[ci] = Object.assign({}, node.children[ci]);
-            node = node.children[ci];
-        }
-        f(node);
-        this.setState({root: newRoot});
+  editAt(path, f) {
+    var newRoot = Object.assign({}, this.state.root);
+    var node = newRoot;
+    for(var i = 0; i < path.length; i++) {
+      const ci = path[i];
+      node.children = Object.assign({}, node.children);
+      node.children[ci] = Object.assign({}, node.children[ci]);
+      node = node.children[ci];
     }
+    f(node);
+    this.setState({root: newRoot});
+  }
 
-    addChild(path) {
-        this.editAt(path, function(node) {
-            node.children = Object.assign({}, node.children);
-            const newPath = path.concat([node.nextFreeChild]);
-            node.children[node.nextFreeChild] = newNode(newPath.toString());
-            node.childrenIds = node.childrenIds.concat([node.nextFreeChild]);
-            node.nextFreeChild += 1;
-        });
-    }
+  addChild(path) {
+    this.editAt(path, function(node) {
+      node.children = Object.assign({}, node.children);
+      const newPath = path.concat([node.nextFreeChild]);
+      node.children[node.nextFreeChild] = newNode(newPath.toString());
+      node.childrenIds = [node.nextFreeChild].concat(node.childrenIds);
+      node.nextFreeChild += 1;
+    });
+  }
 
-    handleKey(path, key) {
-        if(key.key == "Enter") {
-            this.editAt(path, node => node.editing = false);
-        }
+  handleKey(path, key) {
+    if(key.key == "Enter") {
+      this.editAt(path, node => node.editing = false);
     }
+  }
 
-    handleChange(path, change) {
-        const value = change.target.value;
-        this.editAt(path, node => node.content = value);
-    }
+  handleChange(path, change) {
+    const value = change.target.value;
+    this.editAt(path, node => node.content = value);
+  }
 
-    startEditing(path) {
-        this.editAt(path, node => node.editing = true);
-    }
+  startEditing(path) {
+    this.editAt(path, node => node.editing = true);
+  }
 
-    elNodeAt(path) {
-        const node = this.getAt(path);
-        const children = node.childrenIds.map(id => this.elNodeAt(path.concat([id])));
-        return (
-                <ELNode
-            content={node.content}
-            editing={node.editing}
-            children={children}
-            handleKey={key => this.handleKey(path, key)}
-            handleChange={change => this.handleChange(path, change)}
-            startEditing={() => this.startEditing(path)}
-            addChild={() => this.addChild(path)}
-                />
-        );
-    }
+  elNodeAt(path) {
+    const node = this.getAt(path);
+    const children = node.childrenIds.map(id => this.elNodeAt(path.concat([id])));
+    return (
+      <ELNode
+      content={node.content}
+      editing={node.editing}
+      children={children}
+      handleKey={key => this.handleKey(path, key)}
+      handleChange={change => this.handleChange(path, change)}
+      startEditing={() => this.startEditing(path)}
+      addChild={() => this.addChild(path)}
+      />
+    );
+  }
 
-    render() {
-        return this.elNodeAt([]);
-    }
+  render() {
+    return this.elNodeAt([]);
+  }
 }
 
 ReactDOM.render(<EditableList />, document.getElementById('reactContainer'));
